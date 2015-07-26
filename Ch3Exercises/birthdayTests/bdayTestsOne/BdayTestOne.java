ecefe2b53fabc9a65cc7c9e57a43f2a33a0b34b7
Wickk <cbrown347@gmail.com>
2015-07-26 00:54:20 -0400

    Exercise 7.A

---
+++ Ch3Exercises/birthdayTests/bdayTestsOne/BdayTestOne.java
@@ -0,0 +1,46 @@
+/**
+ * 'Chapter 3, Exercise #7.A' How many random people
+ * do you have to select before you find three 
+ * duplicate birthdays. All three people have to be
+ * born on the same day, and month but not necessarily
+ * the same year. 
+ */
+
+public class BdayTestOne {
+	public static void main(String[] args) {
+		int[] used;			// FOr recording the possible birthday
+						// that have been seen so far. A value
+						// of true in used[i] means that a person
+						// whose birthday is the i-th day of the
+						// year has been found. 
+
+		int count;			// The number of people who have been checked
+		int bdayCount;			// Our counter for three birthdays
+		int bdayValue;
+
+		used = new int[366];	// Initially, all entries are false.
+
+		count = 0;
+		bdayCount = 0;
+
+		while (true) {
+				
+			int birthday;		
+			birthday = (int)(Math.random()*365);
+			count++;
+
+			System.out.printf("Person %d has birthday number %d", count, birthday);
+			System.out.println();
+			
+			if ( used[birthday] == 2 ) {
+				break;
+			}
+			used[birthday]++;
+		
+		
+		} // end while loop
+
+		System.out.println();
+		System.out.printf("Found three duplicates after %d tries%n", count);
+	}
+}
